ecada4a70872b48babd139148441c26262590ac8
Or Zidkani <31033101+OrZidkani@users.noreply.github.com>
2023-12-15 16:15:46 +0100

    Update icloud_downloader.sh

--- icloud_downloader.sh
+++ icloud_downloader.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
+# Check if both arguments (username and password) are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <iCloud_username> <iCloud_password>"
+    exit 1
+fi
+
+# Assign arguments to variables
+ICLOUD_USERNAME=$1
+ICLOUD_PASSWORD=$2
+
 # Create a directory for iCloud
-mkdir /var/tmp/iCloud
+mkdir /var/iCloud
 
 # Change to the newly created directory
-cd /var/tmp/iCloud
+cd /var/iCloud
 
-# Download the icloud_photos_downloader from GitHub
+# Download the script
 wget https://github.com/scaraebeus/icloud_photos_downloader/archive/refs/heads/auth_fix.zip
 
 # Unzip the downloaded file
@@ -16,7 +28,5 @@ cd icloud_photos_downloader-auth_fix
 # Build the Docker image
 docker build . -t icloudpd_dev
 
-# Run the Docker container
-# Note: Replace <username> with your iCloud username and <password> with your iCloud password.
-# Replace "/mnt/iCloud" with the path where you want the photos to be saved.
-sudo docker run -it --rm --name icloudpd -v /mnt/iCloud:/data -e TZ=Europe/Berlin icloudpd_dev icloudpd --directory /data --username test@gmail.com --watch-with-interval 1800 --password Mypasword
+# Run the Docker container with the provided username and password
+sudo docker run -it --rm --name icloudpd -v /mnt/iCloud:/data -e TZ=Europe/Berlin icloudpd_dev icloudpd --directory /data --username
